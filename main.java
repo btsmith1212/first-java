d6b549b02c3b4edcb13a52e48b0c84f23fb3a23a
Blake <btsmith1212@gmail.com>
2024-04-11 15:12:03 -0500

    added

--- main.java
+++ main.java
@@ -1,9 +1,25 @@
 public class Main {
     public static void main(String[] args) {
       System.out.println("Hello World");
+      System.out.println("I am learning Java.");
+      System.out.println("It is awesome!");
+      System.out.println(3);
+      System.out.println(358);
+      System.out.println(50000);
+      System.out.println(3 + 3);
+      System.out.println(2 * 5);
+      // Comment
+      /* multi line --
+      -- comment */
+      String name  = "John";
+      System.out.println(name);
+      final int myNum = 15;
+      /// myNum = 20;
+      System.out.println(myNum);
     }
 
   }
+
 
   
 
